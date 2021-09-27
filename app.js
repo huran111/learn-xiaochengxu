da0f9c0fa357ace68fdaa7f83a75156fe29f66a1
huran111 <1345122890@qq.com>
2021-09-27 15:26:29 +0800

    小程序回调函数

--- app.js
+++ app.js
@@ -1,32 +1,59 @@
-//创建app实例的代码以及一些全局相关的内容
-  App({
-  
-    /**
-     * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
-     */
-    onLaunch: function () {
-      
-    },
-  
-    /**
-     * 当小程序启动，或从后台进入前台显示，会触发 onShow
-     */
-    onShow: function (options) {
-      
-    },
-  
-    /**
-     * 当小程序从前台进入后台，会触发 onHide
-     */
-    onHide: function () {
-      
-    },
-  
-    /**
-     * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
-     */
-    onError: function (msg) {
-      console.log(msg);
+//创建app实例的代码以及一些全局相关的内容  全局共享 单例
+new App({
+
+  /**
+   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+   */
+  onLaunch: function () {
+    //网络请求 获取用户信息
+    wx.getUserInfo({
+      success: function (res) {
+        console.log(res.userInfo.nickName)
+        console.log(res)
+      }
+    })
+
+    console.log('小程序完成了初始化')
+  },
+
+  /**
+   * 当小程序启动，或从后台进入前台显示，会触发多次 onShow
+   */
+  onShow: function (options) {
+    console.log("当小程序启动，或从后台进入前台显示，会触发")
+    console.log(options)
+    switch (options.scene) {
+      case 1001:
+        console.log("1001");
+      default:
+        console.log("============");
     }
-  })
-  
+    wx.getUserInfo({
+      success: function(res){
+        console.log(res)
+      }
+    })
+  },
+
+  /**
+   * 当小程序从前台进入后台，会触发 onHide
+   * 后台存货2个小时
+   */
+  onHide: function () {
+    console.log('当小程序从前台进入后台，会触发');
+  },
+
+  /**
+   * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
+   */
+  onError: function (msg) {
+    console.log('当小程序发生脚本错误，或者')
+  },
+  /**
+   * 全局的数据
+   */
+  globalData:{
+    name:'huran',
+    age:18
+  }
+})
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,38 +1,62 @@
 // pages/home/home.js
+//getApp()获取App()产生的示例对象
+const app = getApp()
+const name = app.globalData.name
+const age = app.globalData.age
+//注册一个页面
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-        name:'忽然你',
-        age:23,
-        movies:['a','b','c','d'],
-        students:[
-          {id:110,name:'a',age:12},
-          {id:110,name:'b',age:13}
-        ],
-        counter:0
+    name: '忽然你',
+    age: 23,
+    movies: ['a', 'b', 'c', 'd'],
+    students: [{
+        id: 110,
+        name: 'a',
+        age: 12
+      },
+      {
+        id: 110,
+        name: 'b',
+        age: 13
+      }
+    ],
+    counter: 0,
+    list:''
   },
- 
-  handleBtnClick(){
-      // this.data.counter+=1;
-      // console.log('按钮发生了点击');
-      // console.log(this.data.counter);
+  //获取用户授权
+  handleGetUserInfo(event) {
+    console.log(event)
+  },
+  handleBtnClick() {
+    // this.data.counter+=1;
+    // console.log('按钮发生了点击');
+    // console.log(this.data.counter);
 
-      /**
-       * 下面是正确的写法
-       */
-   
-      this.setData({
-        counter:this.data.counter+=1
-      });
-  },
-  handlerSubtraction(){
+    /**
+     * 下面是正确的写法
+     */
+
     this.setData({
-      counter:this.data.counter-1
+      counter: this.data.counter += 1
     });
   },
+  handlerSubtraction() {
+    this.setData({
+      counter: this.data.counter - 1
+    });
+  },
+  /**
+   * 滚动监听
+   * @param  obj 
+   */
+  onPageScroll(obj){
+    console.log(obj)
+  },
+  
 
 
 
@@ -40,14 +64,25 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+      console.log("onLoad")
+      wx.request({
+        url: 'http://localhost:21999/extinguisher/dDeviceFire/deviceDetails?mac=046b68a1df7c',
+        success:(res)=>{
+          console.log(res)
+          console.log(res.data.data.gpsLocation)
+          const data = res.data.data
+          this.setData({
+            list:data
+          })
+        }
+      })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+      console.log("onReady")
   },
 
   /**
@@ -75,20 +110,26 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+      console.log("监听用户下拉动作")
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    console.log("页面滚动到底部")
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
+
+  },
+  /**
+   * 点击tabbar时触发
+   */
+  onTabItemTap(){
 
   }
 })
