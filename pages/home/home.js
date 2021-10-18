799f503c29248c284e62bbccb093515cdd461b2a
huran111 <1345122890@qq.com>
2021-10-18 16:08:20 +0800

    小程序事件传递

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+      titles:['衣服','裤子','鞋子']
   },
   handleBtnClick(){
     console.log('按钮发生点击')
@@ -40,6 +40,43 @@ Page({
   },
   handleOuter(event){
  console.log(event)
+  },
+
+  handleItemClick(event){
+    console.log(event)
+    const dataset = event.currentTarget.dataset;
+    const title = dataset.item;
+    const index = dataset.index;
+    console.log(title,index);
+  },
+
+
+  /**
+   * 事件捕获和冒泡
+   * 
+   */
+  handlerCaptureView1(){
+    console.log('handlerCaptureView1')
+  },
+  handleBindView1(){
+    console.log('handleBindView1')
+
+  },
+  handlerCaptureView2(){
+    console.log('handlerCaptureView2')
+
+  },
+  handleBindView2(){
+    console.log('handleBindView2')
+
+  },
+  handlerCaptureView3(){
+    console.log('handlerCaptureView3')
+
+  },
+  handleBindView3(){
+    console.log('handleBindView3')
+
   },
   /**
    * 生命周期函数--监听页面加载
